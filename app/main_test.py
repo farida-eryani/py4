06de8bfcc9beeac084a97fa3fcdc6432ad7553fc
farida-eryani <farida.eryani@esante-bretagne.fr>
2023-10-14 23:28:18 +0200

    correction file flask_test.yml encore

--- app/main_test.py
+++ app/main_test.py
@@ -6,7 +6,7 @@ class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
         random_string = "This is my test string"
         random_string_reversed = "gnirts tset ym si sihT"
-        self.assertEqual(random_string reversed,
+        self.assertEqual(random_string_reversed,
                          return_backwards_string(random_string))
         
     def test_get_env(self):
